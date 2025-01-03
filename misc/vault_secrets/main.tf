c354cc6e044c18692a1a1eacf77039fa96eb7023
SathuSid <sisindhriy123@gmail.com>
2025-01-03 07:55:09 +0530

    vault installation

---
+++ misc/vault_secrets/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  backend "s3" {
+    bucket = "terraformpractice007"
+    key    = "roboshop-dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+provider "vault" {
+  address = "https://vault-internal.siddevsecops.icu:8200"
+  token   = var.vault_token
+  skip_tls_verify = true
+}
+
+variable "vault_token" {}
+
+
+resource "vault_mount" "roboshop-dev" {
+  path        = "roboshop-dev"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "RoboShop Dev Secrets"
+}
+
+
+resource "vault_generic_secret" "roboshop-dev" {
+  path = "${vault_mount.roboshop-dev.path}/frontend"
+
+  data_json = <<EOT
+{
+  "foo":   "bar",
+  "pizza": "cheese"
+}
+EOT
+}
