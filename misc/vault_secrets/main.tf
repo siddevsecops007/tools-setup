dfe604ba9468e7e72681213b11f1d2331628ab77
SathuSid <sisindhriy123@gmail.com>
2025-01-03 08:30:01 +0530

    vault installation

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 
 provider "vault" {
-  address = "https://vault-internal.siddevsecops.icu:8200"
+  address = "http://vault-internal.siddevsecops.icu:8200"
   token   = var.vault_token
   skip_tls_verify = true
 }
