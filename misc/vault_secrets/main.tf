ad5ebb0f36bd303ad1a272cccf35b76be7003027
SathuSid <sisindhriy123@gmail.com>
2025-01-03 14:29:49 +0530

    vault installation

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -26,13 +26,37 @@ resource "vault_mount" "roboshop-dev" {
 }
 
 
-resource "vault_generic_secret" "roboshop-dev" {
-  path = "${vault_mount.roboshop-dev.path}/frontend"
+# resource "vault_generic_secret" "roboshop-dev" {
+#   path = "${vault_mount.roboshop-dev.path}/frontend"
+#
+#   data_json = <<EOT
+# {
+#   "foo":   "bar",
+#   "pizza": "cheese"
+# }
+# EOT
+# }
 
+resource "vault_generic_secret" "frontend" {
+  path = "${vault_mount.roboshop-dev.path}/frontend"
   data_json = <<EOT
-{
-  "foo":   "bar",
-  "pizza": "cheese"
+  {
+    "catalogue_url":   "http://catalogue-dev.siddevsecops.icu:8080/",
+    "user_url":   "http://user-dev.siddevsecops.icu:8080/",
+    "cart_url":   "http://cart-dev.siddevsecops.icu:8080/",
+    "shipping_url":   "http://shipping-dev.siddevsecops.icu:8080/",
+    "payment_url":   "http://payment-dev.siddevsecops.icu:8080/"
+
+  }
+  EOT
 }
-EOT
+
+resource "vault_generic_secret" "catalogue" {
+  path = "${vault_mount.roboshop-dev.path}/catalogue"
+  data_json = <<EOT
+  {
+    "MONGO": "true",
+    "MONGO_URL": "mongodb://mongodb-dev.siddevsecops.icu:27017/catalogue"
+  }
+  EOT
 }
