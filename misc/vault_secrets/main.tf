2645727eb33690d7426ca91fc008c5833b36599e
SathuSid <sisindhriy123@gmail.com>
2025-02-20 02:26:24 +0530

    tools setup

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraformpractice007"
+    bucket = "sidterraformpractice007"
     key    = "roboshop-dev/terraform.tfstate"
     region = "us-east-1"
   }
